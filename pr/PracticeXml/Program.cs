e2bcf171760e7a9b0d40ba83d38b0ced9ea5d43a
Azat <46015824+serikovskii@users.noreply.github.com>
2019-03-27 00:09:27 +0600

    practice

---
+++ pr/PracticeXml/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PracticeXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xDoc = new XmlDocument();
+            xDoc.Load("xmlCar.xml");
+            var root = xDoc.DocumentElement;
+            Console.WriteLine(root.LocalName);
+            foreach (XmlAttribute attr in root.Attributes)
+                Console.WriteLine(attr.LocalName);
+            foreach (XmlNode child in root.ChildNodes)
+            {
+                Console.WriteLine("  " + child.Name);
+                foreach (XmlNode childName in child.ChildNodes)
+                {
+                    Console.WriteLine("    " + childName.Name);
+                }
+            }
+                
+            
+
+
+        }
+    }
+}
